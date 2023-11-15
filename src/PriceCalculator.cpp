a6df0ae096525f32cd8ff550557f364ff88d1622
leyang <leyang@github.com>
2023-11-15 13:26:14 +0800

    second

--- src/PriceCalculator.cpp
+++ src/PriceCalculator.cpp
@@ -1,3 +1,42 @@
 #include "PriceCalculator.h"
 
-using namespace PriceCal;
+namespace PriceCal
+{
+    double PriceCalculator::AcceptCash(const DiscountType type, const double money) const noexcept
+    {
+        double cash = 0.0;
+        switch (type)
+        {
+        case DiscountType::CASH_NORMAL:
+        {
+            cash = money;
+            break;
+        }
+        case DiscountType::CASH_90PERCENTOFF:
+        {
+            cash = money * 0.9;
+            break;
+        }
+        case DiscountType::CASH_80PERCENTOFF:
+        {
+            cash = money * 0.8;
+            break;
+        }
+        case DiscountType::CASH_70PERCENTOFF:
+        {
+            cash = money * 0.7;
+            break;
+        }
+        case DiscountType::CASH_BACK:
+        {
+            cash = (money >= 100) ? money - 20 : money;
+            break;
+        }
+        default:
+        {
+            break;
+        }
+        }
+        return cash;
+    }
+}
